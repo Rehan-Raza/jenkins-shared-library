c6cdfc7f9d3d165f57ef7931d29e9480fabe500a
Rehan Raza <70847285+Rehan-Raza@users.noreply.github.com>
2025-03-26 15:11:30 +0530

    Update testUtils.groovy

--- vars/testUtils.groovy
+++ vars/testUtils.groovy
@@ -3,7 +3,7 @@ package org.example
 class TestUtils {
     static void checkTestResults(script) {
         script.sh """
-            errors=\$(grep -oP '(?<=Failures: )\\d+' target/surefire-reports/TEST-summary.txt | awk '{sum += \$1} END {print sum}')
+            errors=\$(grep -oP '(?<=Failures: )\\d+' target/sonar/report-task.txt | awk '{sum += \$1} END {print sum}')
             if [ -z "\$errors" ]; then errors=0; fi
             echo "Total Test Failures: \$errors"
             if [ "\$errors" -ge 6 ]; then
