04718811e293c1cc9c296ba21f21a42e058bfe58
Rehan Raza <70847285+Rehan-Raza@users.noreply.github.com>
2025-03-26 13:22:45 +0530

    Update mvnUtils.groovy

--- vars/mvnUtils.groovy
+++ vars/mvnUtils.groovy
@@ -1,4 +1,8 @@
 // vars/mvnUtils.groovy
-def buildProject() {
-    sh 'mvn clean install -DskipTests'
+def buildProject(String goal) {
+    echo "Building project with goal: ${goal}"
+    sh "mvn clean ${goal}"
 }
+
+// Expose the method to Jenkins pipeline
+return this
