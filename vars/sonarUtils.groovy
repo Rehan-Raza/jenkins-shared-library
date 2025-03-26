9a6921abecad43231d211c2cc776b718480ed8c7
Rehan Raza <70847285+Rehan-Raza@users.noreply.github.com>
2025-03-26 14:21:53 +0530

    Update sonarUtils.groovy

--- vars/sonarUtils.groovy
+++ vars/sonarUtils.groovy
@@ -2,13 +2,13 @@ package org.example
 
 class SonarUtils {
     static void analyzeProject(script) {
-        script.withSonarQubeEnv('sonarqube-9') {
-            script.sh """
-                mvn sonar:sonar \
-                  -Dsonar.projectKey=example-java-maven \
-                  -Dsonar.sources=src/main/java \
-                  -Dsonar.java.binaries=target/classes
-            """
+        script. withSonarQubeEnv('sonarqube-9') {
+                script.sh """
+                        mvn sonar:sonar \
+                          -DargLine="--add-opens java.base/java.lang=ALL-UNNAMED \
+                          --add-opens java.base/java.util=ALL-UNNAMED" \
+                          -Dsonar.java.binaries=target/sonar
+                    """
         }
     }
 }
